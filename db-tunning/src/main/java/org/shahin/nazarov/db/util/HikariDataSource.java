78cde2af3fe53c31c8cbaf5382d579cade10a121
Shahin Nazarov <snazarov@paysis.az>
2019-02-01 19:48:34 +0400

    rabbitmq and db-tunning

---
+++ db-tunning/src/main/java/org/shahin/nazarov/db/util/HikariDataSource.java
@@ -0,0 +1,37 @@
+package org.shahin.nazarov.db.util;
+
+import com.zaxxer.hikari.HikariConfig;
+import lombok.extern.java.Log;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Log
+public class HikariDataSource {
+    private com.zaxxer.hikari.HikariDataSource hikariDataSource;
+
+    public HikariDataSource(){
+        initialize();
+    }
+
+    @PostConstruct
+    public void initialize(){
+        log.info("Data Source Initializing...");
+        HikariConfig config = new HikariConfig("/hikari.properties");
+        hikariDataSource = new com.zaxxer.hikari.HikariDataSource(config);
+    }
+
+
+    @PreDestroy
+    public void releaseResource() {
+        log.info("close data source");
+        if(hikariDataSource != null) {
+            hikariDataSource.close();
+        }
+    }
+
+
+    public com.zaxxer.hikari.HikariDataSource getDataSource() {
+        return hikariDataSource;
+    }
+}
